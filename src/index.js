440277d00d5ced1221cdee0b9d0931405bb8d301
Artur-Alexandrov <arturches@gmail.com>
2019-10-02 11:19:35 +0300

    slution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,18 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    MORSE_TABLE["**********"] = " ";
+    let arr = expr.match(/.{1,10}/g);
+    
+    for(let i = 0; i<arr.length; i++) {
+      arr[i] = arr[i].replace(/11/g, "-");
+      arr[i] = arr[i].replace(/10/g, ".");
+      arr[i] = arr[i].replace(/00/g, " ");
+      arr[i] = arr[i].trimLeft();
+    }  
+    
+    let resArr = arr.map(word => MORSE_TABLE[word])
+    return resArr.join("");
 }
 
 module.exports = {
